d8b429ceb295b7489561ff5a3b1a278cc76ebd71
adhyan11 <77933516+adhyan11@users.noreply.github.com>
2021-06-09 00:59:12 +0530

    Add files via upload

---
+++ piano1.js
@@ -0,0 +1,166 @@
+const keys = document.querySelectorAll(".key")
+keys.forEach(key =>{
+    key.addEventListener("click", () => playNote(key))
+
+})
+function playNote(key){
+    const noteAudio = document.getElementById(key.dataset.note)
+    noteAudio.currentTime=0;
+    noteAudio.play()
+}
+
+
+
+
+
+function lightseagreen(){
+       
+    document.getElementById("myid").style.backgroundColor = "lightseagreen";
+     document.getElementById("myid1").style.backgroundColor = "lightseagreen";
+     document.getElementById("myid2").style.backgroundColor = "lightseagreen";
+     document.getElementById("myid3").style.backgroundColor = "lightseagreen";
+     document.getElementById("myid4").style.backgroundColor = "lightseagreen";
+     document.getElementById("myid5").style.backgroundColor = "lightseagreen";
+     document.getElementById("myid6").style.backgroundColor = "lightseagreen";
+     document.getElementById("myid7").style.backgroundColor = "lightseagreen";
+     document.getElementById("myid8").style.backgroundColor = "lightseagreen";
+     document.getElementById("myid9").style.backgroundColor = "lightseagreen";
+     document.getElementById("myid10").style.backgroundColor = "lightseagreen";
+     document.getElementById("myid11").style.backgroundColor = "lightseagreen";
+     document.getElementById("myid12").style.backgroundColor = "lightseagreen";
+     document.getElementById("myid13").style.backgroundColor = "lightseagreen";
+     document.getElementById("myid14").style.backgroundColor = "lightseagreen";
+     document.getElementById("myid15").style.backgroundColor = "lightseagreen";
+     document.getElementById("myid16").style.backgroundColor = "lightseagreen";
+     document.getElementById("myid17").style.backgroundColor = "lightseagreen";
+     document.getElementById("myid18").style.backgroundColor = "lightseagreen";
+     document.getElementById("myid19").style.backgroundColor = "lightseagreen";
+     document.getElementById("myid20").style.backgroundColor = "lightseagreen";
+    
+    
+   
+    
+}
+
+function lightgoldenyellow(){
+    document.getElementById("myid").style.backgroundColor = "lightgoldenrodyellow";
+   document.getElementById("myid1").style.backgroundColor = "lightgoldenrodyellow";
+     document.getElementById("myid2").style.backgroundColor = "lightgoldenrodyellow";
+     document.getElementById("myid3").style.backgroundColor = "lightgoldenrodyellow";
+     document.getElementById("myid4").style.backgroundColor = "lightgoldenrodyellow";
+     document.getElementById("myid5").style.backgroundColor = "lightgoldenrodyellow";
+     document.getElementById("myid6").style.backgroundColor = "lightgoldenrodyellow";
+     document.getElementById("myid7").style.backgroundColor = "lightgoldenrodyellow";
+     document.getElementById("myid8").style.backgroundColor = "lightgoldenrodyellow";
+     document.getElementById("myid9").style.backgroundColor = "lightgoldenrodyellow";
+     document.getElementById("myid10").style.backgroundColor = "lightgoldenrodyellow";
+     document.getElementById("myid11").style.backgroundColor = "lightgoldenrodyellow";
+     document.getElementById("myid12").style.backgroundColor = "lightgoldenrodyellow";
+     document.getElementById("myid13").style.backgroundColor = "lightgoldenrodyellow";
+     document.getElementById("myid14").style.backgroundColor = "lightgoldenrodyellow";
+     document.getElementById("myid15").style.backgroundColor = "lightgoldenrodyellow";
+     document.getElementById("myid16").style.backgroundColor = "lightgoldenrodyellow";
+     document.getElementById("myid17").style.backgroundColor = "lightgoldenrodyellow";
+     document.getElementById("myid18").style.backgroundColor = "lightgoldenrodyellow";
+     document.getElementById("myid19").style.backgroundColor = "lightgoldenrodyellow";
+     document.getElementById("myid20").style.backgroundColor = "lightgoldenrodyellow";
+
+    
+   
+}
+function lightcoral(){
+    document.getElementById("myid").style.backgroundColor = "lightcoral";
+    document.getElementById("myid1").style.backgroundColor = "lightcoral";
+     document.getElementById("myid2").style.backgroundColor = "lightcoral";
+     document.getElementById("myid3").style.backgroundColor = "lightcoral";
+     document.getElementById("myid4").style.backgroundColor = "lightcoral";
+     document.getElementById("myid5").style.backgroundColor = "lightcoral";
+     document.getElementById("myid6").style.backgroundColor = "lightcoral";
+     document.getElementById("myid7").style.backgroundColor = "lightcoral";
+     document.getElementById("myid8").style.backgroundColor = "lightcoral";
+     document.getElementById("myid9").style.backgroundColor = "lightcoral";
+     document.getElementById("myid10").style.backgroundColor = "lightcoral";
+     document.getElementById("myid11").style.backgroundColor = "lightcoral";
+     document.getElementById("myid12").style.backgroundColor = "lightcoral";
+     document.getElementById("myid13").style.backgroundColor = "lightcoral";
+     document.getElementById("myid14").style.backgroundColor = "lightcoral";
+     document.getElementById("myid15").style.backgroundColor = "lightcoral";
+     document.getElementById("myid16").style.backgroundColor = "lightcoral";
+     document.getElementById("myid17").style.backgroundColor = "lightcoral";
+     document.getElementById("myid18").style.backgroundColor = "lightcoral";
+     document.getElementById("myid19").style.backgroundColor = "lightcoral";
+     document.getElementById("myid20").style.backgroundColor = "lightcoral";
+   
+}
+function lightskyblue(){
+    document.getElementById("myid").style.backgroundColor = "lightskyblue";
+
+   document.getElementById("myid1").style.backgroundColor = "lightskyblue";
+     document.getElementById("myid2").style.backgroundColor = "lightskyblue";
+     document.getElementById("myid3").style.backgroundColor = "lightskyblue";
+     document.getElementById("myid4").style.backgroundColor = "lightskyblue";
+     document.getElementById("myid5").style.backgroundColor = "lightskyblue";
+     document.getElementById("myid6").style.backgroundColor = "lightskyblue";
+     document.getElementById("myid7").style.backgroundColor = "lightskyblue";
+     document.getElementById("myid8").style.backgroundColor = "lightskyblue";
+     document.getElementById("myid9").style.backgroundColor = "lightskyblue";
+     document.getElementById("myid10").style.backgroundColor = "lightskyblue";
+     document.getElementById("myid11").style.backgroundColor = "lightskyblue";
+     document.getElementById("myid12").style.backgroundColor = "lightskyblue";
+     document.getElementById("myid13").style.backgroundColor = "lightskyblue";
+     document.getElementById("myid14").style.backgroundColor = "lightskyblue";
+     document.getElementById("myid15").style.backgroundColor = "lightskyblue";
+     document.getElementById("myid16").style.backgroundColor = "lightskyblue";
+     document.getElementById("myid17").style.backgroundColor = "lightskyblue";
+     document.getElementById("myid18").style.backgroundColor = "lightskyblue";
+     document.getElementById("myid19").style.backgroundColor = "lightskyblue";
+     document.getElementById("myid20").style.backgroundColor = "lightskyblue";
+}
+function lightsteelblue(){
+    document.getElementById("myid").style.backgroundColor = "lightsteelblue";
+document.getElementById("myid1").style.backgroundColor = "lightsteelblue";
+     document.getElementById("myid2").style.backgroundColor = "lightsteelblue";
+     document.getElementById("myid3").style.backgroundColor = "lightsteelblue";
+     document.getElementById("myid4").style.backgroundColor = "lightsteelblue";
+     document.getElementById("myid5").style.backgroundColor = "lightsteelblue";
+     document.getElementById("myid6").style.backgroundColor = "lightsteelblue";
+     document.getElementById("myid7").style.backgroundColor = "lightsteelblue";
+     document.getElementById("myid8").style.backgroundColor = "lightsteelblue";
+     document.getElementById("myid9").style.backgroundColor = "lightsteelblue";
+     document.getElementById("myid10").style.backgroundColor = "lightsteelblue";
+     document.getElementById("myid11").style.backgroundColor = "lightsteelblue";
+     document.getElementById("myid12").style.backgroundColor = "lightsteelblue";
+     document.getElementById("myid13").style.backgroundColor = "lightsteelblue";
+     document.getElementById("myid14").style.backgroundColor = "lightsteelblue";
+     document.getElementById("myid15").style.backgroundColor = "lightsteelblue";
+     document.getElementById("myid16").style.backgroundColor = "lightsteelblue";
+     document.getElementById("myid17").style.backgroundColor = "lightsteelblue";
+     document.getElementById("myid18").style.backgroundColor = "lightsteelblue";
+     document.getElementById("myid19").style.backgroundColor = "lightsteelblue";
+     document.getElementById("myid20").style.backgroundColor = "lightsteelblue";
+   
+}
+function lightpink(){
+    document.getElementById("myid").style.backgroundColor = "lightpink";
+     document.getElementById("myid1").style.backgroundColor = "lightpink";
+     document.getElementById("myid2").style.backgroundColor = "lightpink";
+     document.getElementById("myid3").style.backgroundColor = "lightpink";
+     document.getElementById("myid4").style.backgroundColor = "lightpink";
+     document.getElementById("myid5").style.backgroundColor = "lightpink";
+     document.getElementById("myid6").style.backgroundColor = "lightpink";
+     document.getElementById("myid7").style.backgroundColor = "lightpink";
+     document.getElementById("myid8").style.backgroundColor = "lightpink";
+     document.getElementById("myid9").style.backgroundColor = "lightpink";
+     document.getElementById("myid10").style.backgroundColor = "lightpink";
+     document.getElementById("myid11").style.backgroundColor = "lightpink";
+     document.getElementById("myid12").style.backgroundColor = "lightpink";
+     document.getElementById("myid13").style.backgroundColor = "lightpink";
+     document.getElementById("myid14").style.backgroundColor = "lightpink";
+     document.getElementById("myid15").style.backgroundColor = "lightpink";
+     document.getElementById("myid16").style.backgroundColor = "lightpink";
+     document.getElementById("myid17").style.backgroundColor = "lightpink";
+     document.getElementById("myid18").style.backgroundColor = "lightpink";
+     document.getElementById("myid19").style.backgroundColor = "lightpink";
+     document.getElementById("myid20").style.backgroundColor = "lightpink";
+   
+}
